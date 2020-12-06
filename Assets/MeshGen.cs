5fe6cdc68edd5c4fdf1ecd62ca5652aeb4dffc9c
Laurence Sadler <33729843+lvlzro@users.noreply.github.com>
2020-12-06 08:36:41 -0800

    Height map for cubes working

--- Assets/MeshGen.cs
+++ Assets/MeshGen.cs
@@ -24,41 +24,58 @@ public class MeshGen : MonoBehaviour
     public Vector3[] vertices;
     public int[] triangles;
     public List<Vector3> vertexList = new List<Vector3>();
+    public PerlinGen pn;
+
+    public MeshRenderer testColor;
+    public Texture2D HeightMapTexture2D;
+
+    public int mapRes;
 
 
     // Square square = new Square(Vector3.one, 1);
     private void Start()
     {
+        pn.gridSizeY = height;
+        pn.gridSizeX = width;
         mesh = new Mesh();
         GetComponent<MeshFilter>().mesh = mesh;
+        testColor = GetComponent<MeshRenderer>();
+        pn.Generate();
         CreateVertexGrid();
     }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            mesh.Clear();
+            pn.Generate();
+            CreateVertexGrid();
+            print("new grid");
+        }
+    }
+
 
     void CreateVertexGrid()
     {
         vertices = new Vector3[(width + 1) * (height + 1)];
+        
+        HeightMapTexture2D = new Texture2D(pn.perlinTextureX,pn.perlinTextureY);
 
         for (int z = 0, index = 0; z <= height; z++)
         {
             for (int x = 0; x <= width; x++, index++)
             {
-                var noise = Mathf.PerlinNoise(x * .3f, z * .3f) * 2f;
-                vertices[index] = new Vector3(x, noise, z);
+                var noise = pn.GetSampleFromNoise(x, z);
+                vertices[index] = new Vector3(x, noise * pn.heightScale, z);
+                // HeightMapTexture2D.SetPixel(x,z, pn.grad.Evaluate(pn.GetSampleFromNoise(x, z)));
             }
         }
+        HeightMapTexture2D.Apply();
 
         mesh.vertices = vertices;
 
-        // triangles = new int[6];
-        // triangles[0] = 0;
-        // triangles[1] = width + 1;
-        // triangles[2] = 1;
-        //
-        // triangles[3] = 1;
-        // triangles[4] = width + 1;
-        // triangles[5] = width + 2;
-        // mesh.triangles = triangles;
-
+        testColor.material.mainTexture = HeightMapTexture2D;
         CreateTriangles();
         mesh.RecalculateNormals();
     }
@@ -66,7 +83,7 @@ public class MeshGen : MonoBehaviour
 
     void CreateTriangles()
     {
-        triangles = new int[width *  height * 6];
+        triangles = new int[width * height * 6];
 
         // ---------
 
@@ -76,7 +93,7 @@ public class MeshGen : MonoBehaviour
             for (int x = 0; x < width; x++, triIndex += 6, vertIndex++)
             {
                 triangles[triIndex] = vertIndex;
-                triangles[triIndex + 1] = vertIndex + width + 1;
+                triangles[triIndex + 1] = vertIndex  + width + 1;
                 triangles[triIndex + 2] = vertIndex + 1;
 
                 triangles[triIndex + 3] = vertIndex + 1;
@@ -88,31 +105,5 @@ public class MeshGen : MonoBehaviour
 
         mesh.triangles = triangles;
         Debug.Log("Created Triangles");
-    }
-
-    private void OnDrawGizmos()
-    {
-        // var col = width;
-        // var row = height;
-        // for (int x = 0; x < col; x++)
-        // {
-        //     for (int y = 0; y < row; y++)
-        //     {
-        //         // // Vector3 pos = new Vector3(-width / 2 + x + .5f, 0, -height / 2 + y + +.5f);
-        //         Vector3 pos = new Vector3(x, 0, y);
-        //         // Vector3 size = new Vector3(scale, 0, 1);
-        //         var yRand = Mathf.PerlinNoise(x * .3f, y * .3f) * 2f;
-        //     }
-        // }
-
-
-        // for (int z = 0, i = 0; z < height; z++)
-        // {
-        //     for (int x = 0; x < width; x++, i++)
-        //     {
-        //         Gizmos.color = Color.black;
-        //         Gizmos.DrawSphere(vertices[i], 0.05f);
-        //     }
-        // }
     }
 }
--- Assets/PerlinGen.cs
+++ Assets/PerlinGen.cs
@@ -1,16 +1,25 @@
 ﻿using System;
 using System.Collections;
 using UnityEngine;
+using UnityEngine.PlayerLoop;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 using Random = UnityEngine.Random;
 
 public class PerlinGen : MonoBehaviour
 {
-    private Vector2 perlinOffset;
+    [Header("Perlin Map Set UP")] public int perlinTextureX;
+    public int perlinTextureY;
+    public int gridSizeX;
+    public int gridSizeY;
+    public bool testWithPrefab = false;
+
+    [Header("Map Settings & Location")] public Vector2 perlinOffset;
     private Texture2D perlinTexture2D;
-    public float noiseScale = 1f;
-    private MeshRenderer testImage;
-    public int perlinTextureX, perlinTextureY;
+    public float noiseScale = 1f, heightScale;
+    public MeshRenderer testImage;
+    public GameObject GamePrefab;
+    public Gradient grad;
 
     private void Awake()
     {
@@ -18,36 +27,76 @@ public class PerlinGen : MonoBehaviour
         perlinOffset = new Vector2(Random.Range(0, 99999), Random.Range(0, 99999));
     }
 
-    private void Update()
+    private void Start()
     {
         Generate();
-        testImage.material.mainTexture = perlinTexture2D;
+        if (testWithPrefab) TerrianGen();
     }
 
-    void Generate()
+    private void Update()
     {
-       
+        if (Input.GetKeyDown(KeyCode.R)) SceneManager.LoadScene(0);
+    }
 
+    #region Creating Perlin Data
+
+    public void Generate()
+    {
         perlinTexture2D = new Texture2D(perlinTextureX, perlinTextureY);
 
-       for (int x = 0; x < perlinTextureX; x++) 
+        for (int x = 0; x < perlinTextureX; x++)
         {
             for (int y = 0; y < perlinTextureY; y++)
             {
-                perlinTexture2D.SetPixel(x, y, Sample(x,y));
+                perlinTexture2D.SetPixel(x, y, Sample(x, y)); // write a pixel into an texture
             }
         }
+
         perlinTexture2D.Apply();
+        testImage.material.mainTexture = perlinTexture2D;
     }
 
-    Color Sample(int x, int y)
+    public Color Sample(int x, int y)
     {
-        var xCoord = (float) x / perlinTextureX  * noiseScale+ perlinOffset.x;
-        var yCoord = (float) y / perlinTextureY  * noiseScale+ perlinOffset.y;
+        var xCoord = (float) x / perlinTextureX * noiseScale + perlinOffset.x;
+        var yCoord = (float) y / perlinTextureY * noiseScale + perlinOffset.y;
 
         var sample = Mathf.PerlinNoise(xCoord, yCoord);
 
         Color color = new Color(sample, sample, sample);
         return color;
     }
+
+    #endregion
+
+    #region Generate Terrian
+
+    void TerrianGen()
+    {
+        for (int z = 0; z < gridSizeY; z++)
+        {
+            for (int x = 0; x < gridSizeX; x++)
+            {
+                Vector3 pos = new Vector3(x, GetSampleFromNoise(x, z) * heightScale, z);
+                var cube = Instantiate(GamePrefab, pos, Quaternion.identity);
+                var objectMat = cube.GetComponent<MeshRenderer>();
+                var color = grad.Evaluate(GetSampleFromNoise(x, z));
+                objectMat.material.color = color;
+            }
+        }
+    }
+
+    public float GetSampleFromNoise(int x, int y)
+    {
+        var gridStepX = perlinTextureX / gridSizeX;
+        var gridStepY = perlinTextureY / gridSizeY;
+
+        float sampledNoiseFloat =
+            perlinTexture2D.GetPixel(Mathf.FloorToInt(x * gridStepX), Mathf.FloorToInt(y * gridStepY))
+                .grayscale; // getting the pixel out of the texture, then get the grayscale value (0-1)
+
+        return sampledNoiseFloat;
+    }
+
+    #endregion
 }
